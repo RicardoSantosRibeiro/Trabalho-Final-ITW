eed38fc53f1f846785e742431d8251848c5866c9
Pc <Pc@DESKTOP-520UH6N>
2016-12-05 17:10:49 +0000

    Add project files.

---
+++ App/viewmodels/shell.js
@@ -0,0 +1,15 @@
+﻿define(['plugins/router', 'durandal/app'], function (router, app) {
+    return {
+        router: router,
+        activate: function () {
+            router.map([
+                { route: '', title:'Welcome', moduleId: 'viewmodels/welcome', nav: true, menu : '<i class="fa fa-home"></i>' },
+                { route: 'flickr', moduleId: 'viewmodels/flickr', nav: true, menu: '<i class="fa fa-flickr"></i>' },
+                { route: 'directors', moduleId: 'viewmodels/directors', nav: true, menu: '<i class="fa fa-flickr"></i>' }
+
+            ]).buildNavigationModel();
+            
+            return router.activate();
+        }
+    };
+});
